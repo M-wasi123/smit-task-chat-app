02a8e1f47f9c2efc92d9b49e8bf3c6a92b856024
M-wasi123 <wasikhatri11@gmail.com>
2024-05-28 17:42:28 +0500

    update

--- app.js
+++ app.js
@@ -12,21 +12,34 @@ document.getElementById('file').addEventListener('change', function(event) {
             document.getElementById('ico').style.display = "none"
             fileDisplayArea.style.display= 'block'
 
-        } else if (file.type === 'application/pdf') {
-            var iframe = document.createElement('iframe');
-            iframe.src = reader.result;
-            iframe.width = '100%';
-            iframe.height = '500px';
-            fileDisplayArea.innerHTML = '';
-            fileDisplayArea.appendChild(iframe);
-            document.getElementById('icon').style.display = "none"
-            fileDisplayArea.style.display= 'block'
-        } else {
-            var text = document.createTextNode(file.name + ' (' + file.type + ')');
-            fileDisplayArea.innerHTML = '';
-            fileDisplayArea.appendChild(text);
-        }
+        } 
     };
 
     reader.readAsDataURL(file);
+});
+let userMessage = document.getElementById('user-message')
+let userInput = document.getElementById('user-input');
+userInput.addEventListener('keydown',function () {
+    document.getElementById('send-btn').style.display = 'block'
+    document.getElementsByClassName('icons')[0].style.display = 'none'
+    document.getElementsByClassName('icons')[1].style.display = 'none'
+    document.getElementsByClassName('icons')[2].style.display = 'none'
+
 })
+userInput.addEventListener('keyup',function () {
+    if (userInput.value == '') {
+        document.getElementById('send-btn').style.display = 'none'
+        document.getElementsByClassName('icons')[0].style.display = 'block'
+        document.getElementsByClassName('icons')[1].style.display = 'block'
+        document.getElementsByClassName('icons')[2].style.display = 'block'
+    }
+})
+function keyHandler(event) {
+    if (event.key == 'Enter') {
+        event.preventDefault();
+        document.getElementById('send-btn')
+        userInput.value = ''
+userMessage.innerHTML = `<li class="user">${userInput.value}</li>`
+    }
+}
+document.getElementById('user-input').addEventListener('keydown',keyHandler)
