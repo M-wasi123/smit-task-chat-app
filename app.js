97d02679b25a0840a0ed236d49448b336f1f6672
M-wasi123 <wasikhatri11@gmail.com>
2024-05-30 00:31:20 +0500

    new

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+let userName = document.getElementById('name')
+function register() {
+    // document.getElementsById('form').style.display= 'none'
+    document.getElementById('user-name').innerHTML = userName.value;
+    document.getElementById('message').style.display= 'block'
+}
 document.getElementById('file').addEventListener('change', function(event) {
     var file = event.target.files[0];
     var fileDisplayArea = document.getElementById('fileDisplayArea');
@@ -9,8 +15,6 @@ document.getElementById('file').addEventListener('change', function(event) {
             img.src = reader.result;
             fileDisplayArea.innerHTML = '';
             fileDisplayArea.appendChild(img);
-            document.getElementById('ico').style.display = "none"
-            fileDisplayArea.style.display= 'block'
 
         } 
     };
@@ -19,6 +23,7 @@ document.getElementById('file').addEventListener('change', function(event) {
 });
 let userMessage = document.getElementById('user-message')
 let userInput = document.getElementById('user-input');
+
 userInput.addEventListener('keydown',function () {
     document.getElementById('send-btn').style.display = 'block'
     document.getElementsByClassName('icons')[0].style.display = 'none'
@@ -39,6 +44,25 @@ function keyHandler(event) {
         event.preventDefault();
         document.getElementById('send-btn')
 userMessage.innerHTML += `<li class="user">${userInput.value}</li>`
+if (userInput.value !== '' && userInput.value == 'salam') {
+    userMessage.innerHTML += `<li class='auto-answer'>${messagesObj[0].salam}</li>`
+}
+if (userInput.value = 'salam') {
+    userMessage.innerHTML += `<li class='auto-answer'>${messagesObj[1].userSalam}</li>`
+}
+if (userInput.value == 'alhamdulillah') {
+    userMessage.innerHTML += `<li class='auto-answer'>${messagesObj[3].walaikumSalam}</li>`
+}
+if (userInput.value = 'salam kaisay ho') {
+    userInput.innerHTML += `<li class='auto-answer'>${messagesObj[2].walaikum}</li>`
+}
+if (userInput.value == 'betha hoa hon') {
+    userMessage.innerHTML += `<li class="auto-answer">${messagesObj[4].age}</li>`
+}
+if (userInput.value > 1) {
+    
+}
+
 userInput.value = ''
 }
 }
@@ -46,6 +70,24 @@ function keyHandle(even) {
     even.preventDefault();
     document.getElementById('send-btn')
 userMessage.innerHTML += `<li class="user">${userInput.value}</li>`
+if (userInput.value !== '' && userInput.value == 'salam') {
+    userMessage.innerHTML += `<li class='auto-answer'>${messagesObj[0].salam}</li>`
+}
+if (userInput.value = 'salam') {
+    userMessage.innerHTML += `<li class='auto-answer'>${messagesObj[1].userSalam}</li>`
+}
+if (userInput.value == 'alhamdulillah') {
+    userMessage.innerHTML += `<li class='auto-answer'>${messagesObj[3].walaikumSalam}</li>`
+}
+if (userInput.value = 'salam kaisay ho') {
+    userInput.innerHTML += `<li class='auto-answer'>${messagesObj[2].walaikum}</li>`
+}
+if (userInput.value == 'betha hoa hon') {
+    userMessage.innerHTML += `<li class="auto-answer">${messagesObj[4].age}</li>`
+}
+if (userInput.value > 1) {
+    
+}
 userInput.value = ''
 }
 document.getElementById('user-input').addEventListener('keydown',keyHandler)
@@ -53,6 +95,30 @@ document.getElementById('send-btn').addEventListener('click',keyHandle)
 
 const messagesObj = [
     {
-        salam:`<li class='auto-answer'>Walaikumsalaam Kiya Hal hain ${}`
+        salam:`Salaam Kiya Hal hain`
+    },
+    {
+        userSalam:`Walaikumsalaam kiya hal hain`
+    },
+    {
+        walaikum:`Walaikumsalaam alhamdulilah`
+    },
+    {
+        walaikumSalam:`Kia ker rahay ho`
+    },
+    {
+age:`Age kiya hay aap ki`
+    },
+    {
+        goods:`App ko sab say ziada kon sa fruit pasand hay`
+    },
+    {
+        nice:`Ye waqai buht acha hay nice pasand`
+    },
+    {
+time:`Time pass mat kero may complain ker don ga`
+    },
+    {
+        koi:`Koi kaam dhanda nahi kia tumharay pass`
     }
 ]
