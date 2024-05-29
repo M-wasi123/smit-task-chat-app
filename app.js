2cfbdba717a6ade3496648a8d251643c7a16af97
M-wasi123 <wasikhatri11@gmail.com>
2024-05-30 02:41:10 +0500

    new

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let userName = document.getElementById('name')
 function register() {
-    // document.getElementsById('form').style.display= 'none'
-    document.getElementById('user-name').innerHTML = userName.value;
-    document.getElementById('message').style.display= 'block'
+    if (userName.value !== '' ) {
+        document.getElementById('user-name').innerHTML = userName.value;
+        document.getElementById('message').style.display= 'block'
+    }else{
+        alert('Please enter your name')
+    }
 }
 document.getElementById('file').addEventListener('change', function(event) {
     var file = event.target.files[0];
@@ -15,7 +18,8 @@ document.getElementById('file').addEventListener('change', function(event) {
             img.src = reader.result;
             fileDisplayArea.innerHTML = '';
             fileDisplayArea.appendChild(img);
-
+document.getElementById('ico').style.display = 'none'
+fileDisplayArea.style.display= 'block'
         } 
     };
 
@@ -43,82 +47,77 @@ function keyHandler(event) {
     if (event.key == 'Enter') {
         event.preventDefault();
         document.getElementById('send-btn')
+        autoScroll()
 userMessage.innerHTML += `<li class="user">${userInput.value}</li>`
-if (userInput.value !== '' && userInput.value == 'salam') {
-    userMessage.innerHTML += `<li class='auto-answer'>${messagesObj[0].salam}</li>`
+if (userInput.value == 'hellow'||userInput.value == 'hi') {
+    userMessage.innerHTML += `<li class='auto-answer'>How are you</li>`
+    userInput.value = ''
+}if (userInput.value == 'i am fine') {
+        userMessage.innerHTML += `<li class='auto-answer'>What is your age</li>`
+        userInput.value = ''
+}if (userInput.value > 18) {
+        userMessage.innerHTML += `<li class='auto-answer'>You are young</li>`
+        userInput.value = ''
+}if (userInput.value < 18 && userInput.value > 0) {
+        userMessage.innerHTML += `<li class='auto-answer'>You are children</li>`
+        userInput.value = ''
 }
-if (userInput.value = 'salam') {
-    userMessage.innerHTML += `<li class='auto-answer'>${messagesObj[1].userSalam}</li>`
-}
-if (userInput.value == 'alhamdulillah') {
-    userMessage.innerHTML += `<li class='auto-answer'>${messagesObj[3].walaikumSalam}</li>`
-}
-if (userInput.value = 'salam kaisay ho') {
-    userInput.innerHTML += `<li class='auto-answer'>${messagesObj[2].walaikum}</li>`
-}
-if (userInput.value == 'betha hoa hon') {
-    userMessage.innerHTML += `<li class="auto-answer">${messagesObj[4].age}</li>`
-}
-if (userInput.value > 1) {
-    
+if (userInput.value > 49) {
+        userMessage.innerHTML += `<li class='auto-answer'>You are old man</li>`
+        userInput.value = ''
+}else{
+        userMessage.innerHTML += `<li class='auto-answer'>${message}</li>`
+        userInput.value = ''
 }
 
-userInput.value = ''
 }
 }
 function keyHandle(even) {
     even.preventDefault();
     document.getElementById('send-btn')
+    autoScroll()
 userMessage.innerHTML += `<li class="user">${userInput.value}</li>`
-if (userInput.value !== '' && userInput.value == 'salam') {
-    userMessage.innerHTML += `<li class='auto-answer'>${messagesObj[0].salam}</li>`
+if (userInput.value == 'hellow'||userInput.value == 'hi') {
+    userMessage.innerHTML += `<li class='auto-answer'>How are you</li>`
+    userInput.value = ''
+}if (userInput.value == 'i am fine') {
+        userMessage.innerHTML += `<li class='auto-answer'>What is your age</li>`
+        userInput.value = ''
+}if (userInput.value > 17) {
+        userMessage.innerHTML += `<li class='auto-answer'>You are young</li>`
+        userInput.value = ''
+}if (userInput.value < 18) {
+        // userMessage.innerHTML += `<li class='auto-answer'>You are children</li>`
+        userInput.value = ''
+}if (userInput.value > 49) {
+        userMessage.innerHTML += `<li class='auto-answer'>You are old man</li>`
+        userInput.value = ''
+}else{
+        userMessage.innerHTML += `<li class='auto-answer'>${message}</li>`
+        userInput.value = ''
 }
-if (userInput.value = 'salam') {
-    userMessage.innerHTML += `<li class='auto-answer'>${messagesObj[1].userSalam}</li>`
-}
-if (userInput.value == 'alhamdulillah') {
-    userMessage.innerHTML += `<li class='auto-answer'>${messagesObj[3].walaikumSalam}</li>`
-}
-if (userInput.value = 'salam kaisay ho') {
-    userInput.innerHTML += `<li class='auto-answer'>${messagesObj[2].walaikum}</li>`
-}
-if (userInput.value == 'betha hoa hon') {
-    userMessage.innerHTML += `<li class="auto-answer">${messagesObj[4].age}</li>`
-}
-if (userInput.value > 1) {
-    
-}
-userInput.value = ''
+
 }
 document.getElementById('user-input').addEventListener('keydown',keyHandler)
 document.getElementById('send-btn').addEventListener('click',keyHandle)
 
 const messagesObj = [
-    {
-        salam:`Salaam Kiya Hal hain`
-    },
-    {
-        userSalam:`Walaikumsalaam kiya hal hain`
-    },
-    {
-        walaikum:`Walaikumsalaam alhamdulilah`
-    },
-    {
-        walaikumSalam:`Kia ker rahay ho`
-    },
-    {
-age:`Age kiya hay aap ki`
-    },
-    {
-        goods:`App ko sab say ziada kon sa fruit pasand hay`
-    },
-    {
-        nice:`Ye waqai buht acha hay nice pasand`
-    },
-    {
-time:`Time pass mat kero may complain ker don ga`
-    },
-    {
-        koi:`Koi kaam dhanda nahi kia tumharay pass`
-    }
-]
+    { as: 'Kiya ker rahay ho' },
+    { au: 'Khana khaliya' },
+    { hy: 'May samjha nahi kiya khay rahay hain' },
+    { tb: 'Tabyet to theek hay app ki' },
+    { me: 'Mujhay tang mat kero' },
+    { nr: 'May sonay jaraha hon' }
+ ];
+ 
+ let randomMessage = messagesObj[Math.floor(Math.random() * messagesObj.length)];
+ 
+ let key = Object.keys(randomMessage);
+ let message = randomMessage[key];
+ message.reload()
+//  console.log(`Message: ${message}`);
+// console.log(arr)
+// console.log(messagesObj.value)
+function autoScroll() {
+    userMessage.scrollTo(0, 10000)
+}
